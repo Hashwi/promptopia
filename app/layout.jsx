c4ea373fd6163513997fde5092de920b84d3ed6e
Hashwi <119130028+Hashwi@users.noreply.github.com>
2023-10-04 13:00:35 +0200

    Implement Auth

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,5 @@
 import "@styles/globals.css";
-import Nav from '../components/Nav'
+import Nav from "../components/Nav";
 import Provider from "../components/Provider";
 
 export const metadate = {
@@ -11,13 +11,15 @@ const RootLayout = ({ children }) => {
   return (
     <html lang="en">
       <body>
-        <div className="main">
-          <div className="gradient" />
-        </div>
-        <main className="app">
-            <Nav/>
+        <Provider>
+          <div className="main">
+            <div className="gradient" />
+          </div>
+          <main className="app">
+            <Nav />
             {children}
-            </main>
+          </main>
+        </Provider>
       </body>
     </html>
   );
---
+++ utils/database.js
@@ -0,0 +1,7 @@
+import mongoose from "mongoose";
+
+let isConnected = fales
+
+export const connectToBD = async () => {
+    mongoose.set('strictQuery'),
+}
