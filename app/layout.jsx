ce404785dad88282eadaaea8205892276c47adff
Hashwi <119130028+Hashwi@users.noreply.github.com>
2023-10-04 11:15:12 +0200

    Add mobile navigation

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,25 +1,26 @@
-import '@styles/globals.css'
-
+import "@styles/globals.css";
+import Nav from '../components/Nav'
+import Provider from "../components/Provider";
 
 export const metadate = {
-   title: "Promptopia",
-   decription: "Discover & Share AI Prompts"
-}
+  title: "Promptopia",
+  decription: "Discover & Share AI Prompts",
+};
 
-const RootLayout = ({children}) => {
+const RootLayout = ({ children }) => {
   return (
-    <html lang='en'>
-        <body>
-            <div className='main'>
-                <div className='gradient'/>
-            </div>
-            <main className='app'>
-                {children}
+    <html lang="en">
+      <body>
+        <div className="main">
+          <div className="gradient" />
+        </div>
+        <main className="app">
+            <Nav/>
+            {children}
             </main>
-        </body>
-
+      </body>
     </html>
-  )
+  );
 };
 
 export default RootLayout;
