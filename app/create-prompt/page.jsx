991ea7062924a5ed6a72ea3b67a1e419ae40cd82
Hashwi <119130028+Hashwi@users.noreply.github.com>
2023-10-05 10:50:02 +0200

    Connecting to the database

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -1,30 +1,48 @@
-'use client'
-import React,{useState} from "react";
+"use client";
+import React, { useState } from "react";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 
-import Form from '@components/Form'
+import Form from "@components/Form";
 
 const CreatePrompt = () => {
-  const [submitting, setSubmitting] = useState(false)
+  const [submitting, setSubmitting] = useState(false);
   const [post, setpost] = useState({
-    prompt: '',
-    tag: '',
-  })
+    prompt: "",
+    tag: "",
+  });
 
   const createPrompt = async (e) => {
-
-  }
+    e.preventDefault();
+    setSubmitting(true);
+    try {
+      const response = await fetch("/api/prompt/new", {
+        method: "POST",
+        body: JSON.stringify({
+          prompt: post.prompt,
+          userId: session?.user.id,
+          tag: post.tag,
+        }),
+      });
+      if (response.ok) {
+        Router.push("/");
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      submitting(false);
+    }
+  };
 
   return (
-    <Form 
-    type ="Create"
-    post={post}
-    setpost={setpost}
-    submitting={submitting}
-    handlesubmit={createPrompt}
+    <Form
+      type="Create"
+      post={post}
+      setpost={setpost}
+      submitting={submitting}
+      handlesubmit={createPrompt}
     />
-  )
+  );
 };
 
 export default CreatePrompt;
