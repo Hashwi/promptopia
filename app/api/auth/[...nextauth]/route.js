6a543f9f6d05567118585da8b0accf7233a8a405
Hashwi <119130028+Hashwi@users.noreply.github.com>
2023-10-05 14:57:17 +0200

    Fetching the data from database

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,46 +1,47 @@
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
-import { connectToDB } from '@utils/database';
-import User from '@models/user';
 
-// Establish database connection
-connectToDB();
+import User from '@models/user';
+import { connectToDB } from '@utils/database';
 
 const handler = NextAuth({
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    }),
+    })
   ],
-
-  // Optional: Define additional callbacks
   callbacks: {
-    // Session callback
     async session({ session }) {
-        const sessionUser = await User.findOne({
-            email: session.user.email
-        })
+      // store the user id from MongoDB to session
+      const sessionUser = await User.findOne({ email: session.user.email });
+      session.user.id = sessionUser._id.toString();
 
-        session.user.id = sessionUser.id.toString();
-
-        return session;
+      return session;
     },
-    async signIn({ profile }) {
-      // Check if a user already exists
-      const userExists = await User.findOne({ email: profile.email });
+    async signIn({ account, profile, user, credentials }) {
+      try {
+        await connectToDB();
 
-      if (!userExists) {
-        await User.create({
-          email: profile.email,
-          username: profile.name.replace(/ /g, '').toLowerCase(),
-          image: profile.picture,
-        });
+        // check if user already exists
+        const userExists = await User.findOne({ email: profile.email });
+
+        // if not, create a new document and save user in MongoDB
+        if (!userExists) {
+          await User.create({
+            email: profile.email,
+            username: profile.name.replace(" ", "").toLowerCase(),
+            image: profile.picture,
+          });
+        }
+
+        return true
+      } catch (error) {
+        console.log("Error checking if user exists: ", error.message);
+        return false
       }
-
-      return true; // Allow sign in
     },
-  },
-});
+  }
+})
 
-export { handler as GET, handler as POST };
+export { handler as GET, handler as POST }
--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -1,20 +1,22 @@
 "use client";
-import React, { useState } from "react";
+
+import { useState } from "react";
 import { useSession } from "next-auth/react";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 
 import Form from "@components/Form";
 
 const CreatePrompt = () => {
-  const [submitting, setSubmitting] = useState(false);
-  const [post, setpost] = useState({
-    prompt: "",
-    tag: "",
-  });
+  const router = useRouter();
+  const { data: session } = useSession();
+
+  const [submitting, setIsSubmitting] = useState(false);
+  const [post, setPost] = useState({ prompt: "", tag: "" });
 
   const createPrompt = async (e) => {
     e.preventDefault();
-    setSubmitting(true);
+    setIsSubmitting(true);
+
     try {
       const response = await fetch("/api/prompt/new", {
         method: "POST",
@@ -24,23 +26,24 @@ const CreatePrompt = () => {
           tag: post.tag,
         }),
       });
+
       if (response.ok) {
-        Router.push("/");
+        router.push("/");
       }
     } catch (error) {
       console.log(error);
     } finally {
-      submitting(false);
+      setIsSubmitting(false);
     }
   };
 
   return (
     <Form
-      type="Create"
+      type='Create'
       post={post}
-      setpost={setpost}
+      setPost={setPost}
       submitting={submitting}
-      handlesubmit={createPrompt}
+      handleSubmit={createPrompt}
     />
   );
 };
